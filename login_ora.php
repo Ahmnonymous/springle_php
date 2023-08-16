e0c1aa12d18f0d69a82c57454f7876cfb79598ee
ahmnonymous <a4medqureshi8@gmail.com>
2023-08-17 02:15:15 +0500

    springle updates

--- login_ora.php
+++ login_ora.php
@@ -2,7 +2,7 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      include_once "db_ora.php";
+    include_once "db_ora.php"; // Include your Oracle database connection file
 
     $email = $_POST["email"];
     $password = $_POST["password"];
@@ -10,31 +10,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($email) || empty($password)) {
         $_SESSION['error_message'] = "All fields are required";
         header("Location: index.php");
+        exit();
     }
 
-    $sql = "SELECT * FROM country_master WHERE email = '$email'";
-    $result = $conn->query($sql);
+    // Oracle-specific: Construct the SQL statement
+    $sql = "SELECT * FROM country_master WHERE email = :email";
 
-    if($result->num_rows === 1) {
-        $row = $result->fetch_assoc();
-        $hashedPassword = $row["password"];
+    // Prepare the statement
+    $stmt = oci_parse($conn, $sql);
 
-        if (password_verify($password, $hashedPassword)) {
-            $_SESSION['auth'] = true;
-            header("Location: form.php");
-            exit();
+    // Bind the parameters
+    oci_bind_by_name($stmt, ':email', $email);
+
+    // Execute the statement
+    $result = oci_execute($stmt);
+
+    if ($result) {
+        $row = oci_fetch_assoc($stmt);
+        if ($row) {
+            $hashedPassword = $row["PASSWORD"];
+            if (password_verify($password, $hashedPassword)) {
+                $_SESSION['auth'] = true;
+                header("Location: form.php");
+                exit();
+            } else {
+                $_SESSION['error_message'] = "Invalid login details";
+                header("Location: index.php");
+                exit();
+            }
         } else {
-            $_SESSION['error_message']= "Invalid login details";
+            $_SESSION['error_message'] = "Invalid login details";
             header("Location: index.php");
-
+            exit();
         }
-    } 
-    else{
-        $_SESSION['error_message'] = "Invalid login details";
+    } else {
+        $_SESSION['error_message'] = "Error executing query";
         header("Location: index.php");
+        exit();
     }
 
+    // Free the statement
+    oci_free_statement($stmt);
 
+    oci_close($conn); // Close Oracle connection
 }
 ?>
-
