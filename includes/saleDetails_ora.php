b933a8b6ac73b9c6c627391243d207a24c3af6e5
ahmnonymous <a4medqureshi8@gmail.com>
2023-08-17 00:35:10 +0500

    springle updates

--- includes/saleDetails_ora.php
+++ includes/saleDetails_ora.php
@@ -14,20 +14,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $date = $_POST["date"];
   $amount = $quantity * $rate;
 
-  // Use prepared statement to prevent SQL injection
-  $sql = "INSERT INTO sale_detail (book_id, sal_id, customer_name, bot_issue, Bot_recived, Rate, Amount, Bot_balance, Pay_recived, to_date) 
-          VALUES (2237, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-  
-  $stmt = $conn->prepare($sql);
-  $stmt->bind_param("ssiiidiids", $sal_id, $customer_name, $quantity, $bot_rec, $rate, $amount, $bot_balance, $pay_received, $date);
+  // Escape values to prevent SQL injection
+  $sal_id = mysqli_real_escape_string($conn, $sal_id);
+  $customer_name = mysqli_real_escape_string($conn, $customer_name);
+  $quantity = intval($quantity);
+  $bot_rec = intval($bot_rec);
+  $bot_balance = intval($bot_balance);
+  $pay_received = intval($pay_received);
+  $rate = floatval($rate);
+  $amount = floatval($amount);
+  $contact = intval($contact);
+  $date = mysqli_real_escape_string($conn, $date);
 
-  if ($stmt->execute()) {
+  $sql = "INSERT INTO sale_detail (book_id, sal_id, customer_name, bot_issue, Bot_recived, Rate, Amount, Bot_balance, Pay_recived, to_date) 
+          VALUES (2237, '$sal_id', '$customer_name', $quantity, $bot_rec, $rate, $amount, $bot_balance, $pay_received, '$date')";
+
+  if ($conn->query($sql) === TRUE) {
     $_SESSION['success_message'] = "Record inserted successfully!";
   } else {
-    $_SESSION['error_message'] = "Error inserting record: " . $stmt->error;
+    $_SESSION['error_message'] = "Error inserting record: " . $conn->error();
   }
-
-  $stmt->close();
 } else {
   echo "Server method is not POST.";
 }
