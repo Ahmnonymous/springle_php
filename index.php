9c5f0c9f636826351b70572b06ff4f4fefe46ceb
ahmnonymous <a4medqureshi8@gmail.com>
2023-08-16 22:28:05 +0500

    springle updates

--- index.php
+++ index.php
@@ -5,68 +5,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="jquery-ui.css">
-  <link rel="stylesheet" href="includes/style.css">
+  <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.24/sweetalert2.all.css">
-  <style>
-    body {
-      background-color: #f5f4f4;
-    }
-
-    .container {
-      margin-top: 4rem;
-    }
-
-    .custom-form {
-      max-width: 400px;
-      margin: 0 auto;
-      padding: 2rem;
-      background-color: white;
-      border-radius: 5px;
-      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-    }
-
-    .input-text {
-      width: 100%;
-      padding: 0.5rem;
-      border: none;
-      border-bottom: 1px solid #ccc;
-      font-size: 16px;
-    }
-
-    .label {
-      position: absolute;
-      top: 0.75rem;
-      left: 1rem;
-      font-size: 16px;
-      color: #666;
-      pointer-events: none;
-      transition: 0.2s all;
-    }
-
-    .input-text:focus ~ .label,
-    .input-text.not-empty ~ .label {
-      top: -1rem;
-      font-size: 12px;
-      color: #007bff;
-    }
-
-    .submit-btn {
-      width: 100%;
-      padding: 0.75rem;
-      background-color: #007bff;
-      border: none;
-      color: white;
-      font-weight: bold;
-      cursor: pointer;
-      transition: background-color 0.2s;
-    }
-
-    .submit-btn:hover {
-      background-color: #0056b3;
-    }
-  </style>
+  
 </head>
 
 <body>
