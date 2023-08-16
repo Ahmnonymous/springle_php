cfe58686bd0daf85857ae8d1b2dfa24bdd9480b3
ahmnonymous <a4medqureshi8@gmail.com>
2023-08-16 22:21:23 +0500

    springle updates

--- index.php
+++ index.php
@@ -10,62 +10,7 @@
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.24/sweetalert2.all.css">
   <style>
-    body {
-      background-color: #f5f4f4;
-    }
 
-    .container {
-      margin-top: 4rem;
-    }
-
-    .custom-form {
-      max-width: 400px;
-      margin: 0 auto;
-      padding: 2rem;
-      background-color: white;
-      border-radius: 5px;
-      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-    }
-
-    .input-text {
-      width: 100%;
-      padding: 0.5rem;
-      border: none;
-      border-bottom: 1px solid #ccc;
-      font-size: 16px;
-    }
-
-    .label {
-      position: absolute;
-      top: 0.75rem;
-      left: 1rem;
-      font-size: 16px;
-      color: #666;
-      pointer-events: none;
-      transition: 0.2s all;
-    }
-
-    .input-text:focus ~ .label,
-    .input-text.not-empty ~ .label {
-      top: -1rem;
-      font-size: 12px;
-      color: #007bff;
-    }
-
-    .submit-btn {
-      width: 100%;
-      padding: 0.75rem;
-      background-color: #007bff;
-      border: none;
-      color: white;
-      font-weight: bold;
-      cursor: pointer;
-      transition: background-color 0.2s;
-    }
-
-    .submit-btn:hover {
-      background-color: #0056b3;
-    }
   </style>
 </head>
 
@@ -78,12 +23,10 @@
         <div class="form-field col-sm-12 mx-auto">
           <input id="email" name="email" placeholder="email" class="input-text js-input form-control shadow-none rounded-0" type="email"
             required>
-          <!--label class="label" for="email">Email</label-->
         </div>
         <div class="form-field col-sm-12 mx-auto">
           <input id="password" name="password" placeholder="password" class="input-text js-input form-control shadow-none rounded-0"
             type="password" required>
-          <!--label class="label" for="password">Password</label-->
         </div>
         <div class="form-field col-sm-12 text-center">
           <input class="submit-btn btn btn-primary" type="submit" value="Login">
