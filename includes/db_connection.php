592d1e15ddd371b1c85127167b565b836a087c5f
ahmnonymous <a4medqureshi8@gmail.com>
2023-08-16 19:17:56 +0500

    springle updates

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,15 +1,12 @@
 <?php
 $host = 'localhost';
 $username = 'springle_admin';
-$password = '786$karacHi';
+$password = 'TlCc9b358L@o(S';
 $dbname = 'springle_craft';
 
 // Establish a database connection
 $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
-  echo 'erro';
   die("Connection failed: " . $conn->connect_error);
 }
-else
-echo 'success';
 ?>
