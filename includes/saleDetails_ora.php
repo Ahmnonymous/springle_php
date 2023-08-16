8860337c91f428f0081a391f30f1a5ed6069c70b
ahmnonymous <a4medqureshi8@gmail.com>
2023-08-17 00:37:09 +0500

    springle updates

--- includes/saleDetails_ora.php
+++ includes/saleDetails_ora.php
@@ -14,30 +14,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $date = $_POST["date"];
   $amount = $quantity * $rate;
 
-  // Escape values to prevent SQL injection
-  $sal_id = mysqli_real_escape_string($conn, $sal_id);
-  $customer_name = mysqli_real_escape_string($conn, $customer_name);
-  $quantity = intval($quantity);
-  $bot_rec = intval($bot_rec);
-  $bot_balance = intval($bot_balance);
-  $pay_received = intval($pay_received);
-  $rate = floatval($rate);
-  $amount = floatval($amount);
-  $contact = intval($contact);
-  $date = mysqli_real_escape_string($conn, $date);
+  $escaped_sal_id = oci_escape_string($conn, $sal_id);
+  $escaped_customer_name = oci_escape_string($conn, $customer_name);
+  $escaped_quantity = intval($quantity);
+  $escaped_bot_rec = intval($bot_rec);
+  $escaped_bot_balance = intval($bot_balance);
+  $escaped_pay_received = intval($pay_received);
+  $escaped_rate = floatval($rate);
+  $escaped_amount = floatval($amount);
+  $escaped_contact = intval($contact);
+  $escaped_date = oci_escape_string($conn, $date);
 
   $sql = "INSERT INTO sale_detail (book_id, sal_id, customer_name, bot_issue, Bot_recived, Rate, Amount, Bot_balance, Pay_recived, to_date) 
-          VALUES (2237, '$sal_id', '$customer_name', $quantity, $bot_rec, $rate, $amount, $bot_balance, $pay_received, '$date')";
+          VALUES (2237, '$escaped_sal_id', '$escaped_customer_name', $escaped_quantity, $escaped_bot_rec, $escaped_rate, $escaped_amount, $escaped_bot_balance, $escaped_pay_received, '$escaped_date')";
 
-  if ($conn->query($sql) === TRUE) {
+  $query = oci_parse($conn, $sql);
+  $result = oci_execute($query);
+
+  if ($result) {
     $_SESSION['success_message'] = "Record inserted successfully!";
   } else {
-    $_SESSION['error_message'] = "Error inserting record: " . $conn->error();
+    $_SESSION['error_message'] = "Error inserting record.";
   }
+
+  oci_free_statement($query);
 } else {
   echo "Server method is not POST.";
 }
 
-$conn->close();
+oci_close($conn);
 header("Location: ../form.php");
 ?>
