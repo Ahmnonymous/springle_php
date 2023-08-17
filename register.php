6a6b770612a64673d3e677125399772b16bb3fa4
ahmnonymous <a4medqureshi8@gmail.com>
2023-08-17 12:54:56 +0500

    springle updates

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="js/jquery-ui.css">
+  <!--link rel="stylesheet" href="js/jquery-ui.css"-->
   <link rel="stylesheet" href="css/style.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.24/sweetalert2.all.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -47,8 +47,8 @@
 </div>
 
   </div>
-  <script src="js/jquery.js"></script>
-  <script src="js/jquery-ui.js"></script>
+  <!--script src="js/jquery.js"></script-->
+  <!--script src="js/jquery-ui.js"></script-->
   <script>
     $('.js-input').keyup(function() {
       if ($(this).val()) {
