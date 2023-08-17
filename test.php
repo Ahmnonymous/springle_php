475bdda0bd904ba5cc4b8c90692e609a959ff0e1
ahmnonymous <a4medqureshi8@gmail.com>
2023-08-18 02:07:43 +0500

    springle updates

--- test.php
+++ test.php
@@ -15,7 +15,7 @@ if (!$conn) {
 //$usernam = $_SESSION['username'];
 
 $sql = "SELECT T.CLAINT_ID, T.NAME, T.rate, T.Bot_bal, T.pay_bal, T.mobile, t.ref_id FROM DATA_FATCH_VIEW T
-          WHERE T.active ='1' and T.REF='Javed'";
+          WHERE T.active ='1' ";
 
 $stid = oci_parse($conn, $sql);
 if (!$stid) {
